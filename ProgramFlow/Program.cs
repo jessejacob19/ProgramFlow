f3461183341c27eac21ba4c4a55ec1464c2d0d51
jesse collier <jesse.collier97@gmail.com>
2019-06-20 17:44:55 +1200

    basic game without loop

--- ProgramFlow/Program.cs
+++ ProgramFlow/Program.cs
@@ -6,7 +6,28 @@ namespace ProgramFlow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int ourNumber = 10;
+
+            Console.WriteLine("Please guess the number");
+            string answer = Console.ReadLine();
+            int guessNumber = 0;
+            Int32.TryParse(answer, out guessNumber);
+
+            if (guessNumber == ourNumber)
+            {
+                Console.WriteLine("Correct! Well Done!");
+            } else if (guessNumber > ourNumber)
+            {
+                Console.WriteLine("Too High!!");
+            } else if(guessNumber < ourNumber) {
+                Console.WriteLine("Too Low!!");
+            } else
+            {
+                Console.WriteLine("Sorry, thats wrong!");
+            }
+
+            Console.WriteLine("Press any key to end the program");
+            Console.ReadLine();
         }
     }
 }
